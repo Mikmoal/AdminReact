2c8cce378083e62cf75ff530bc5a669909eabec1
Mikmoal <miguel.moam@outlook.com>
2023-05-03 16:02:20 -0600

    fullcalendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,10 +1,11 @@
 import { SimpleAccordion } from "../sections/calendar/calendar-juntas.tsx";
 import { BasicDateCalendar } from "../sections/calendar/calendar-days.js";
+import { DemoApp } from "../sections/calendar/calendar-full.tsx";
 const Calendar = () => {
   return (
     <>
       <SimpleAccordion></SimpleAccordion>
-      <BasicDateCalendar></BasicDateCalendar>
+      <DemoApp></DemoApp>
     </>
   );
 };
--- src/sections/calendar/calendar-days.js
+++ src/sections/calendar/calendar-days.js
@@ -1,12 +1,23 @@
-import * as React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
 export function BasicDateCalendar() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
-    </LocalizationProvider>
+    // <LocalizationProvider dateAdapter={AdapterDayjs}>
+    //   <DateCalendar />
+    // </LocalizationProvider>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      weekends={false}
+      events={[
+        { title: "event 1", date: "2019-04-01" },
+        { title: "event 2", date: "2019-04-02" },
+      ]}
+    />
   );
 }
