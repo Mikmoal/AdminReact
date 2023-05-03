c687d7082fbf72c8ea4ae9a30014cbcb2b97cb87
Mikmoal <miguel.moam@outlook.com>
2023-05-03 01:42:41 -0600

    minutas

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,27 +1,11 @@
-import { BasicDateCalendar } from "src/sections/calendar/calendar";
+import { SimpleAccordion } from "../sections/calendar/calendar-juntas.tsx";
+import { BasicDateCalendar } from "../sections/calendar/calendar-days.js";
 const Calendar = () => {
-  // const [page, setPage] = useState(0);
-  // const [rowsPerPage, setRowsPerPage] = useState(5);
-  // const customers = useCustomers(page, rowsPerPage);
-  // const customersIds = useCustomerIds(customers);
-  // const customersSelection = useSelection(customersIds);
-
-  // const handlePageChange = useCallback(
-  //   (event, value) => {
-  //     setPage(value);
-  //   },
-  //   []
-  // );
-
-  // const handleRowsPerPageChange = useCallback(
-  //   (event) => {
-  //     setRowsPerPage(event.target.value);
-  //   },
-  //   []
-  // );
-
   return (
-    <BasicDateCalendar></BasicDateCalendar>
+    <>
+      <SimpleAccordion></SimpleAccordion>
+      <BasicDateCalendar></BasicDateCalendar>
+    </>
   );
 };
 
